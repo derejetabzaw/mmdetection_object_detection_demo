12cfba129af86a0fb53860fc303578cdde667ce5
Dereje <derejetabzaw@gmail.com>
2022-07-30 15:56:23 +0300

    Updated Datasets

---
+++ convert_images.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+
+images_path = os.path.join('dataset', 'JPEGImages')
+ext = "jpg"
+for root, dirs, files in os.walk(images_path):
+    for filename in files:
+        if filename.endswith(".png"):
+            image = cv2.imread(os.path.join(root, filename))
+            new_fname = "{}.{}".format(os.path.splitext(filename)[0], ext)
+            small_fname = os.path.join(root, new_fname)
+            cv2.imwrite(small_fname, image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
