676969e6c74efb6b7e0554807d50ad3f9129b1e7
Dereje <derejetabzaw@gmail.com>
2022-09-24 16:38:03 +0300

    chevron dataset

--- convert_images.py
+++ convert_images.py
@@ -5,7 +5,7 @@ images_path = os.path.join('data/VOC2007', 'JPEGImages')
 ext = "jpg"
 for root, dirs, files in os.walk(images_path):
     for filename in files:
-        if filename.endswith(".png"):
+        if filename.endswith(".jpeg"):
             image = cv2.imread(os.path.join(root, filename))
             new_fname = "{}.{}".format(os.path.splitext(filename)[0], ext)
             small_fname = os.path.join(root, new_fname)
