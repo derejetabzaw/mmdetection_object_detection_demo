c4c171c836a2b2053a0b3f3536a26545c9694b86
Dereje <derejetabzaw@gmail.com>
2022-10-11 22:43:04 +0300

    New Training Set

--- convert_images.py
+++ convert_images.py
@@ -1,12 +1,15 @@
 import os
 import cv2
 
-images_path = os.path.join('data/VOC2007', 'JPEGImages')
-ext = "jpg"
+images_path = os.path.join('../tapes', '')
+desired_ext = "jpg"
+convert_ext = [".jpeg" , ".png"] 
 for root, dirs, files in os.walk(images_path):
     for filename in files:
-        if filename.endswith(".jpeg"):
+        if filename.endswith(tuple(convert_ext)):
             image = cv2.imread(os.path.join(root, filename))
-            new_fname = "{}.{}".format(os.path.splitext(filename)[0], ext)
+            new_fname = "{}.{}".format(os.path.splitext(filename)[0], desired_ext)
             small_fname = os.path.join(root, new_fname)
             cv2.imwrite(small_fname, image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+
